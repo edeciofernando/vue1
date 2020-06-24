6996b00544799c31dd534e1584eecf3a3b9c99df
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2020-06-24 09:53:08 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 
 Vue.config.productionTip = false
 // Cria uma variável "global" com a URL do WebService
-Vue.prototype.$urlAPI = 'http://127.0.0.1:5000'
+// Vue.prototype.$urlAPI = 'http://127.0.0.1:5000'
+Vue.prototype.$urlAPI = 'http://edeciofernando.pythonanywhere.com'
 
 new Vue({
   router,
